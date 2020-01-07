48835f74abae3175d39026030c8c8d64e67f0404
Anshu <raianshu8991@gmail.com>
2020-01-07 09:15:54 -0500

    port changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const cookieParser = require('cookie-parser');
 
 const app=express();
-const port=process.env.port;
+const port=process.env.Port;
 const passport=require('passport');
 const session=require('express-session');
 const passportlocal=require('./config/passport-local');
