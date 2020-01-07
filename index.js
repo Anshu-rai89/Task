8e423bb7f852f34af993d40958bc46d7dfed72f9
Anshu <raianshu8991@gmail.com>
2020-01-07 06:04:14 -0500

    update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(sassMiddleware({
     src: './assets/scss',
     dest: './assets/css',
     debug: true,
-    outputStyle: 'extended',
+    outputStyle: 'compressed',
     prefix: '/css'
 }));
 app.use(express.urlencoded());
